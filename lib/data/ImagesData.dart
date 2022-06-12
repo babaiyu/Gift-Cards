3ca8d190d3109a7246308c4168df31f58d67dffe
babaiyu <bayuputra9a@gmail.com>
2022-06-12 21:32:02 +0700

    Created dummy data for ImagesData

---
+++ lib/data/ImagesData.dart
@@ -0,0 +1,15 @@
+class ImagesData {
+  int index;
+  String imageAsset;
+
+  ImagesData({
+    required this.index,
+    required this.imageAsset,
+  });
+}
+
+var imagesData = [
+  ImagesData(index: 1, imageAsset: 'images/card_1.jpg'),
+  ImagesData(index: 2, imageAsset: 'images/card_2.jpg'),
+  ImagesData(index: 3, imageAsset: 'images/card_3.jpg'),
+];
