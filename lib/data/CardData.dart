a84c28b463d49716d46281a13da81cd7c0ab0b9f
babaiyu <bayuputra9a@gmail.com>
2022-06-13 14:17:36 +0700

    Created CardData

---
+++ lib/data/CardData.dart
@@ -0,0 +1,15 @@
+class CardData {
+  String title;
+  String description;
+  String cardFrom;
+  String cardFor;
+  String imageAsset;
+
+  CardData({
+    required this.title,
+    required this.description,
+    required this.cardFrom,
+    required this.cardFor,
+    required this.imageAsset,
+  });
+}
