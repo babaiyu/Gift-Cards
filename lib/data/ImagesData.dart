a6cf34a42d403721e9ce30111a63b5622ae88a9b
babaiyu <bayuputra9a@gmail.com>
2022-06-13 15:12:32 +0700

    Add new images card

--- lib/data/ImagesData.dart
+++ lib/data/ImagesData.dart
@@ -12,4 +12,7 @@ var imagesData = [
   ImagesData(index: 1, imageAsset: 'images/card_1.jpg'),
   ImagesData(index: 2, imageAsset: 'images/card_2.jpg'),
   ImagesData(index: 3, imageAsset: 'images/card_3.jpg'),
+  ImagesData(index: 4, imageAsset: 'images/card_4.jpg'),
+  ImagesData(index: 5, imageAsset: 'images/card_5.jpg'),
+  ImagesData(index: 6, imageAsset: 'images/card_6.jpg'),
 ];
